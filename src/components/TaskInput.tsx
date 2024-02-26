4a647ad41912e25da007907cea75852dfef60e4e
HitalloAzevedo <azevedohitallo@gmail.com>
2024-02-26 16:01:35 -0300

    add done props and create new task through enter key

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -5,6 +5,7 @@ import { PlusCircle } from 'phosphor-react'
 interface Task {
     id: number;
     content: string;
+    done: boolean;
 }
 
 interface Handler {
@@ -25,12 +26,23 @@ export function TaskInput({ handleNewTask }: Handler) {
                 onChange={(e) => {
                     setTaskContent(e.target.value)
                 }}
+                onKeyDown={(key) => {
+                    if (key.code == 'Enter') {
+                        if (taskContent != '') {
+                            handleNewTask({id: id, content: taskContent, done: false})
+                            setId((state) => {
+                                return state + 1
+                            })
+                        }
+                        setTaskContent('')
+                    }
+                }}
             />
             <button 
                 className={styles.newTaskButton}
                 onClick={() => {
                     if (taskContent != '') {
-                        handleNewTask({id: id, content: taskContent})
+                        handleNewTask({id: id, content: taskContent, done: false})
                         setId((state) => {
                             return state + 1
                         })
