7793ab0bd77a5c0662db34fc977b0ca97ec800db
HitalloAzevedo <azevedohitallo@gmail.com>
2024-02-25 23:55:21 -0300

    tasks area empty done

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Header } from './components/Header'
 import { TaskInput } from './components/TaskInput'
+import clipboard from './assets/clipboard.svg'
 
 
 function App() {
@@ -8,6 +9,19 @@ function App() {
     <>
       <Header/>
       <TaskInput/>
+
+      <div className='tasksArea'>
+        <header>
+          <p className='createdTasks'>Tarefas criadas <span>0</span></p>
+          <p className='doneTasks'>Concluídas <span>0</span></p>
+        </header>
+
+        <div className='tasks'>
+          <img src={clipboard}/>
+          <p><strong>Você ainda não tem tarefas cadastradas</strong></p>
+          <p>Crie tarefas e organize seus itens a fazer</p>
+        </div>
+      </div>
     </>
   )
 }
