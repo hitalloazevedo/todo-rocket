189a102d03370e5d0f9540a31ce0267a27454fe5
HitalloAzevedo <azevedohitallo@gmail.com>
2024-02-26 14:57:20 -0300

    do a task and delete task feature

--- src/components/Task.tsx
+++ src/components/Task.tsx
@@ -1,16 +1,40 @@
 import styles from './Task.module.css'
 import { Trash } from 'phosphor-react'
 
-export function Task() {
+interface TaskProps {
+    content: string;
+    deleteTask: (task: string) => void;
+}
+
+export function Task(props: TaskProps) {
+
     return (
         <div className={styles.task}>
             <label className={styles.container}>
-                <input type="checkbox" />
+                <input 
+                    type="checkbox" 
+                    onClick={(e) => {
+                        if (e.target.checked) {
+                            e.target.parentNode.parentNode.querySelector('p').classList.toggle('taskDone')
+                        } else {
+                            e.target.parentNode.parentNode.querySelector('p').classList.toggle('taskDone')
+                        }
+                    }}
+                />
                 <span className={styles.checkmark}></span>
             </label>
-            <p>content Lorem ipsum dolor sit amet consecteturDeserunt accusamus impedit non neo nostrum, odio debitis impedit nam explicabo vero adipisci eos quae fugit inventore.
-            </p>
-            <Trash className={styles.trashIcon} size={24}/>
+            <p>{props.content}</p>
+            <Trash 
+                className={styles.trashIcon} 
+                size={24}
+                onClick={(e) => {
+                    if (e.target.classList.contains('_trashIcon_if5gv_49')){
+                        props.deleteTask(e.target.parentNode.querySelector('p').textContent)
+                    } else {
+                        props.deleteTask(e.target.parentNode.parentNode.parentNode.querySelector('p').textContent)
+                    }
+            }}
+            />
         </div>
     )
 }
