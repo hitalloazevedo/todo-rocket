3adc3ee5d0882b6d1e7b2eb9c7500ac8f7b9695c
HitalloAzevedo <azevedohitallo@gmail.com>
2024-02-26 14:56:33 -0300

    feat: add new task feature

--- src/components/TaskInput.tsx
+++ src/components/TaskInput.tsx
@@ -1,11 +1,44 @@
+import { useState } from 'react';
 import styles from './TaskInput.module.css'
 import { PlusCircle } from 'phosphor-react'
 
-export function TaskInput() {
+interface Task {
+    id: number;
+    content: string;
+}
+
+interface Handler {
+    handleNewTask: (task: Task) => void;
+}
+
+export function TaskInput({ handleNewTask }: Handler) {
+
+    const [taskContent, setTaskContent] = useState('')
+    const [id, setId] = useState(0)
+
     return(
         <div className={styles.newTask}>
-            <input type="text" placeholder='Adicione uma nova tarefa' />
-            <button className={styles.newTaskButton}>Criar <PlusCircle size={16}/></button>
+            <input 
+                type="text" 
+                placeholder='Adicione uma nova tarefa' 
+                value={taskContent}
+                onChange={(e) => {
+                    setTaskContent(e.target.value)
+                }}
+            />
+            <button 
+                className={styles.newTaskButton}
+                onClick={() => {
+                    if (taskContent != '') {
+                        handleNewTask({id: id, content: taskContent})
+                        setId((state) => {
+                            return state + 1
+                        })
+                    }
+                    setTaskContent('')
+                }}
+                >Criar<PlusCircle size={16}/>
+            </button>
         </div>
     )
 }
